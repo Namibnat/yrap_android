bcc9616c0654b333072c85fa30f4d820c868edb2
Vernon Swanepoel <frantic.naturalist@gmail.com>
2022-01-10 05:13:43 +0000

    Hope page strings set in string.xml

--- app/src/main/java/co/uk/yrap/yrap/MainActivity.kt
+++ app/src/main/java/co/uk/yrap/yrap/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import android.content.Intent
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
